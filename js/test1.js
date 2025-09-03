cb26cc2cd4e5265b35e6d3ff6480107577068cd0
AntonioBotis <botisantonio@gmail.com>
2025-09-03 23:06:23 +0300

    test3

--- js/test1.js
+++ js/test1.js
@@ -1,4 +1,15 @@
-alert("Hello World");
+
+/*alert("Hello World");
 let aux=true;
 console.log(aux);
 console.log(typeof aux);
+*/
+
+
+const an=2024;
+const viitor= an + 8;
+const trecut= an-7;
+console.log(an, viitor, trecut);
+let x=5;
+x*=22;
+console.log(x); 
