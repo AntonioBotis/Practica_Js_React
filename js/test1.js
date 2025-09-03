91095268bcc2a76121abd4d11df85aef2eeb4375
AntonioBotis <botisantonio@gmail.com>
2025-09-03 23:17:18 +0300

    challenge#1 section2

--- js/test1.js
+++ js/test1.js
@@ -6,10 +6,41 @@ console.log(typeof aux);
 */
 
 
-const an=2024;
+
+/*const an=2024;
 const viitor= an + 8;
 const trecut= an-7;
 console.log(an, viitor, trecut);
 let x=5;
 x*=22;
 console.log(x); 
+
+console.log(x>an);
+
+//test1
+const mass_mark=78;
+const height_mark=1.69;
+const mass_john=92;
+const height_john=1.95;
+
+const bmi_mark=mass_mark/(height_mark**2);
+const bmi_john=mass_john/(height_john**2);
+
+const mark_higher_bmi=bmi_mark>bmi_john;
+console.log(bmi_mark, bmi_john, mark_higher_bmi);
+
+//test2
+const mass_mark=95;
+const height_mark=1.88;
+const mass_john=85;
+const height_john=1.76;
+
+const bmi_mark=mass_mark/(height_mark**2);
+const bmi_john=mass_john/(height_john**2);
+
+const mark_higher_bmi=bmi_mark>bmi_john;
+console.log(bmi_mark, bmi_john, mark_higher_bmi);*/
+
+
+
+
