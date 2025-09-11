d5c87a30589f06a4b58a82421ba2447544f69b40
AntonioBotis <botisantonio@gmail.com>
2025-09-11 16:09:39 +0300

    before s7 ch1

--- js/section7_project1/s7p1.js
+++ js/section7_project1/s7p1.js
@@ -1,14 +1,52 @@
 'use strict';
-
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent='Marian Bonctiu';
 
 document.querySelector('.score').textContent=25;
-document.querySelector('.highscore').textContent=15;
+document.querySelector('.highscore').textContent=1;
 
-document.querySelector('.guess').value=99;
-console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value=99;*/
 
-document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value)});
+
+const number=Math.trunc(Math.random()*20)+5;
+let score=20;
+
+
+document.querySelector('.check').addEventListener('click', function() 
+{
+    const guess= Number(document.querySelector('.guess').value);
+  
+
+
+    if(!guess)
+    {
+        document.querySelector('.message').textContent='No input detected';
+        
+
+    }
+    else if(guess===number & score>0)
+    {
+        document.querySelector('.message').textContent='Correct number';
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+        document.querySelector('.number').textContent=number;
+    
+    }
+    else if(guess>number & score>0)
+    {
+        document.querySelector('.message').textContent='Above the number';
+        document.querySelector('.score').textContent=--score;
+    }
+    else if( guess<number & score>0)   
+    {
+        document.querySelector('.message').textContent='Too low bruv';
+        document.querySelector('.score').textContent=--score;
+    }
+    else if(score==0){
+        document.querySelector('.message').textContent='Game over';
+         document.querySelector('body').style.backgroundColor='#ff0000ff';
+        stop;
+    }
+
+});
