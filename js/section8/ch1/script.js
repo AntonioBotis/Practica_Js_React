8b9aa9120d628d5ca49163fed768b37aa4c59ccf
AntonioBotis <botisantonio@gmail.com>
2025-09-20 23:13:19 +0300

    section9 ch3

--- js/section8/ch1/script.js
+++ js/section8/ch1/script.js
@@ -6,7 +6,7 @@
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored*/
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 const game = {
   team1: 'Bayern Munich',
@@ -78,7 +78,7 @@ printGoals(...game.scored);
 
 
 team1<team2 && console.log('Team 1');
-team1>team2 && console.log('team2');*/
+team1>team2 && console.log('team2');
 let i=0;
 for (const goal_name of game.scored) {
   i++;
@@ -114,6 +114,109 @@ for(const goal_name of game.scored)
 console.log(arr);
 
 
+const order=new Set(['a','b','b','a']);
+console.log(order);
+console.log(new Set('marian'));
+console.log(order.size);
+
+
 
   
-  
+  const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+const common=italianFoods.intersection(mexicanFoods);
+console.log(...common);
+
+const all=italianFoods.union(mexicanFoods);
+console.log(...all);
+*/
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const arr=[];
+let nrEvents=0;
+const events2=[...new Set(gameEvents.values())];
+console.log(...events2);
+
+for (const element of gameEvents) {
+  nrEvents+=1;
+  arr.push(element[1]);
+  element[0]<=45 ? console.log(`[First Half] ${element[0]} : ${element[1]}`):console.log(`[Second Half] ${element[0]} : ${element[1]}`);
+}
+const events=new Set(arr);
+console.log(...events);
+
+gameEvents.delete(64);
+
+console.log(gameEvents);
+
+console.log(Math.ceil(90/nrEvents));
+
+
