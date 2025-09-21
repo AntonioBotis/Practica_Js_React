b90ce0236890c5a9d408a6f3498760c22d7b1183
AntonioBotis <botisantonio@gmail.com>
2025-09-21 18:25:42 +0300

    section9 ch4

--- js/section8/ch1/script.js
+++ js/section8/ch1/script.js
@@ -184,8 +184,6 @@ Let's continue with our football betting app! This time, we have a map with a lo
       [FIRST HALF] 17: ⚽️ GOAL
 
 GOOD LUCK 😀
-*/
-
 const gameEvents = new Map([
   [17, '⚽️ GOAL'],
   [36, '🔁 Substitution'],
@@ -217,6 +215,50 @@ gameEvents.delete(64);
 
 console.log(gameEvents);
 
-console.log(Math.ceil(90/nrEvents));
+console.log(Math.ceil(90/nrEvents));*/
+
+// Coding Challenge #4
+
+
+/*Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+
+
+document.querySelector('button').addEventListener('click',function()
+{let text= document.querySelector('textarea').value;
+  text=text.toLowerCase();
+ 
+  const underIndex=text.indexOf('_');
+  text=text.slice(0,underIndex)+text[underIndex+1].toUpperCase()+text.slice(underIndex+2,text.length);
+  console.log(text);
+});
 
 
