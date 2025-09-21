11a5b11d302c9e6b5d095f7dda56d6ef3a2c1a70
AntonioBotis <botisantonio@gmail.com>
2025-09-21 18:51:44 +0300

    array_practice

--- js/section8/ch1/script.js
+++ js/section8/ch1/script.js
@@ -245,9 +245,7 @@ HINT 4: This challenge is difficult on purpose, so start watching the solution i
 
 Afterwards, test with your own test data!
 
-GOOD LUCK 😀*/
-
-document.body.append(document.createElement('textarea'));
+GOOD LUCK 😀document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 
@@ -260,5 +258,43 @@ document.querySelector('button').addEventListener('click',function()
   text=text.slice(0,underIndex)+text[underIndex+1].toUpperCase()+text.slice(underIndex+2,text.length);
   console.log(text);
 });
+*/
+
+/*
+///////////////////////////////////////
+// String Methods Practice
 
 
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+*/
+
+
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+let organized=flights.trim().split('+');
+console.log(organized);
+
+const cut= function(flight){
+
+flight=flight.trim().split(';');
+flight[0]=flight[0].slice(1,flight[0].length);
+flight[0]=flight[0].replace('_',' ');
+
+flight[1]=flight[1].slice(0,3).toUpperCase();
+flight[2]=flight[2].slice(0,3).toUpperCase();
+flight[3]=flight[3].replace(':','h');
+
+
+console.log(`${flight[0]} from ${flight[1]} to ${flight[2]} (${flight[3]})`);
+
+}
+
+for (const element of organized) {
+  cut(element);
+}
